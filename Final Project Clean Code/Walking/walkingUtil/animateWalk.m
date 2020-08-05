3b2760a0d01b522857ed0aa4f020be98fd64e6e9
ealauer <42587703+ealauer@users.noreply.github.com>
2020-08-05 15:21:58 -0400

    i think this is the last file i missed

---
+++ Final Project Clean Code/Walking/walkingUtil/animateWalk.m
@@ -0,0 +1,162 @@
+%% Animate Walk - By Ethan Lauer
+% This function plots the animation of the walking robot and saves it as a
+% gif. Leg 1 is red, leg 2 is green, leg 3 is dark blue, leg 4 is light
+% blue.
+%
+% Input:timeMat - time matrix of one cycle
+%         listOfJntPosLeg1- list of leg 1's joint positions during the
+%                       full cycle
+%         listOfJntPosLeg2- list of leg 2's joint positions during the
+%                       full cycle
+%         listOfJntPosLeg3- list of leg 3's joint positions during the
+%                       full cycle
+%         listOfJntPosLeg4 - list of leg 4's joint positions during the
+%                       full cycle
+%        Title - string for the title of the figure of the stickplot
+%                (ex. 'stickplot')
+%        filename - string of the file name to save the gif as (must end in
+%                   '.gif' ex. 'pose_to_pose.gif')
+%         limits - 3x2 matrix indicating the upper and lower limits of the
+%                   axes of the plot
+
+function [] =animateWalk(timeMat,listOfJntPosLeg1,listOfJntPosLeg2,listOfJntPosLeg3,listOfJntPosLeg4, Title, fileName, limits)
+fig = figure('Name', Title);
+grid on
+xlim(limits(1,:))
+ylim(limits(2,:))
+zlim(limits(3,:))
+
+hold on
+xlabel('X Position (in)');
+zlabel('Z Position (in)');
+ylabel('Y Position (in)');
+title(Title)
+filename= fileName;
+view(70,45);
+
+for i=1:length(timeMat)    
+    %% 
+    % Plot joint and feet points in space
+    
+    % Leg 1
+    currHipJntPointLeg1=scatter3(listOfJntPosLeg1(1,i),listOfJntPosLeg1(2,i),listOfJntPosLeg1(3,i),'MarkerFaceColor','r');
+    currKneeJntPointLeg1=scatter3(listOfJntPosLeg1(4,i),listOfJntPosLeg1(5,i),listOfJntPosLeg1(6,i),'MarkerFaceColor','r');
+    currAnkleJntPointLeg1=scatter3(listOfJntPosLeg1(7,i),listOfJntPosLeg1(8,i),listOfJntPosLeg1(9,i),'MarkerFaceColor','r');
+    currFootJntPointLeg1=scatter3(listOfJntPosLeg1(10,i),listOfJntPosLeg1(11,i),listOfJntPosLeg1(12,i),'MarkerFaceColor','r');
+    
+    % Leg 2
+    currHipJntPointLeg2=scatter3(listOfJntPosLeg2(1,i),listOfJntPosLeg2(2,i),listOfJntPosLeg2(3,i),'MarkerFaceColor','g');
+    currKneeJntPointLeg2=scatter3(listOfJntPosLeg2(4,i),listOfJntPosLeg2(5,i),listOfJntPosLeg2(6,i),'MarkerFaceColor','g');
+    currAnkleJntPointLeg2=scatter3(listOfJntPosLeg2(7,i),listOfJntPosLeg2(8,i),listOfJntPosLeg2(9,i),'MarkerFaceColor','g');
+    currFootJntPointLeg2=scatter3(listOfJntPosLeg2(10,i),listOfJntPosLeg2(11,i),listOfJntPosLeg2(12,i),'MarkerFaceColor','g');
+    
+    % Leg 3
+    currHipJntPointLeg3=scatter3(listOfJntPosLeg3(1,i),listOfJntPosLeg3(2,i),listOfJntPosLeg3(3,i),'MarkerFaceColor','b');
+    currKneeJntPointLeg3=scatter3(listOfJntPosLeg3(4,i),listOfJntPosLeg3(5,i),listOfJntPosLeg3(6,i),'MarkerFaceColor','b');
+    currAnkleJntPointLeg3=scatter3(listOfJntPosLeg3(7,i),listOfJntPosLeg3(8,i),listOfJntPosLeg3(9,i),'MarkerFaceColor','b');
+    currFootJntPointLeg3=scatter3(listOfJntPosLeg3(10,i),listOfJntPosLeg3(11,i),listOfJntPosLeg3(12,i),'MarkerFaceColor','b');
+    
+    % Leg 4
+    currHipJntPointLeg4=scatter3(listOfJntPosLeg4(1,i),listOfJntPosLeg4(2,i),listOfJntPosLeg4(3,i),'MarkerFaceColor','c');
+    currKneeJntPointLeg4=scatter3(listOfJntPosLeg4(4,i),listOfJntPosLeg4(5,i),listOfJntPosLeg4(6,i),'MarkerFaceColor','c');
+    currAnkleJntPointLeg4=scatter3(listOfJntPosLeg4(7,i),listOfJntPosLeg4(8,i),listOfJntPosLeg4(9,i),'MarkerFaceColor','c');
+    currFootJntPointLeg4=scatter3(listOfJntPosLeg4(10,i),listOfJntPosLeg4(11,i),listOfJntPosLeg4(12,i),'MarkerFaceColor','c');
+    
+    
+    % put points in array so easier to delete
+    jntPointArr = [currHipJntPointLeg1,currKneeJntPointLeg1,currAnkleJntPointLeg1,...
+        currHipJntPointLeg2,currKneeJntPointLeg2,currAnkleJntPointLeg2,...
+        currHipJntPointLeg3,currKneeJntPointLeg3,currAnkleJntPointLeg3,...
+        currHipJntPointLeg4,currKneeJntPointLeg4,currAnkleJntPointLeg4,...
+        currFootJntPointLeg1,currFootJntPointLeg2, currFootJntPointLeg3, currFootJntPointLeg4];
+    %% 
+    % Plot the lines connecting the joints
+    
+    % connect hips
+    hip1To2Line = plot3([listOfJntPosLeg1(1,i);listOfJntPosLeg2(1,i)],...
+        [listOfJntPosLeg1(2,i);listOfJntPosLeg2(2,i)],...
+        [listOfJntPosLeg1(3,i);listOfJntPosLeg2(3,i)],'r-');
+    hip2To4Line = plot3([listOfJntPosLeg2(1,i);listOfJntPosLeg4(1,i)],...
+        [listOfJntPosLeg2(2,i);listOfJntPosLeg4(2,i)],...
+        [listOfJntPosLeg2(3,i);listOfJntPosLeg4(3,i)],'r-');
+    hip3To4Line = plot3([listOfJntPosLeg3(1,i);listOfJntPosLeg4(1,i)],...
+        [listOfJntPosLeg3(2,i);listOfJntPosLeg4(2,i)],...
+        [listOfJntPosLeg3(3,i);listOfJntPosLeg4(3,i)],'r-');
+    hip3To1Line = plot3([listOfJntPosLeg3(1,i);listOfJntPosLeg1(1,i)],...
+        [listOfJntPosLeg3(2,i);listOfJntPosLeg1(2,i)],...
+        [listOfJntPosLeg3(3,i);listOfJntPosLeg1(3,i)],'r-');
+    
+    % connect hips to knees
+    hipToKneeLineLeg1 = plot3([listOfJntPosLeg1(1,i);listOfJntPosLeg1(4,i)],...
+        [listOfJntPosLeg1(2,i);listOfJntPosLeg1(5,i)],...
+        [listOfJntPosLeg1(3,i);listOfJntPosLeg1(6,i)],'r-');
+    hipToKneeLineLeg2 = plot3([listOfJntPosLeg2(1,i);listOfJntPosLeg2(4,i)],...
+        [listOfJntPosLeg2(2,i);listOfJntPosLeg2(5,i)],...
+        [listOfJntPosLeg2(3,i);listOfJntPosLeg2(6,i)],'r-');
+    hipToKneeLineLeg3 = plot3([listOfJntPosLeg3(1,i);listOfJntPosLeg3(4,i)],...
+        [listOfJntPosLeg3(2,i);listOfJntPosLeg3(5,i)],...
+        [listOfJntPosLeg3(3,i);listOfJntPosLeg3(6,i)],'r-');
+    hipToKneeLineLeg4 = plot3([listOfJntPosLeg4(1,i);listOfJntPosLeg4(4,i)],...
+        [listOfJntPosLeg4(2,i);listOfJntPosLeg4(5,i)],...
+        [listOfJntPosLeg4(3,i);listOfJntPosLeg4(6,i)],'r-');
+    
+    
+    % connect knees to ankles
+    kneeToAnkleLineLeg1 = plot3([listOfJntPosLeg1(7,i);listOfJntPosLeg1(4,i)],...
+        [listOfJntPosLeg1(8,i);listOfJntPosLeg1(5,i)],...
+        [listOfJntPosLeg1(9,i);listOfJntPosLeg1(6,i)],'r-');
+    kneeToAnkleLineLeg2 = plot3([listOfJntPosLeg2(7,i);listOfJntPosLeg2(4,i)],...
+        [listOfJntPosLeg2(8,i);listOfJntPosLeg2(5,i)],...
+        [listOfJntPosLeg2(9,i);listOfJntPosLeg2(6,i)],'r-');
+    kneeToAnkleLineLeg3 = plot3([listOfJntPosLeg3(7,i);listOfJntPosLeg3(4,i)],...
+        [listOfJntPosLeg3(8,i);listOfJntPosLeg3(5,i)],...
+        [listOfJntPosLeg3(9,i);listOfJntPosLeg3(6,i)],'r-');
+    kneeToAnkleLineLeg4 = plot3([listOfJntPosLeg4(7,i);listOfJntPosLeg4(4,i)],...
+        [listOfJntPosLeg4(8,i);listOfJntPosLeg4(5,i)],...
+        [listOfJntPosLeg4(9,i);listOfJntPosLeg4(6,i)],'r-');
+    
+    % connect ankles to feet
+    ankleToFeetLineLeg1 = plot3([listOfJntPosLeg1(7,i);listOfJntPosLeg1(10,i)],...
+        [listOfJntPosLeg1(8,i);listOfJntPosLeg1(11,i)],...
+        [listOfJntPosLeg1(9,i);listOfJntPosLeg1(12,i)],'r-');
+    ankleToFeetLineLeg2 = plot3([listOfJntPosLeg2(7,i);listOfJntPosLeg2(10,i)],...
+        [listOfJntPosLeg2(8,i);listOfJntPosLeg2(11,i)],...
+        [listOfJntPosLeg2(9,i);listOfJntPosLeg2(12,i)],'r-');
+   ankleToFeetLineLeg3 = plot3([listOfJntPosLeg3(7,i);listOfJntPosLeg3(10,i)],...
+        [listOfJntPosLeg3(8,i);listOfJntPosLeg3(11,i)],...
+        [listOfJntPosLeg3(9,i);listOfJntPosLeg3(12,i)],'r-');
+    ankleToFeetLineLeg4 = plot3([listOfJntPosLeg4(7,i);listOfJntPosLeg4(10,i)],...
+        [listOfJntPosLeg4(8,i);listOfJntPosLeg4(11,i)],...
+        [listOfJntPosLeg4(9,i);listOfJntPosLeg4(12,i)],'r-');
+    
+    % put in array so easier to delete afterwards
+    lineArr = [hip1To2Line,hip2To4Line,hip3To4Line hip3To1Line,...
+        hipToKneeLineLeg1, hipToKneeLineLeg2, hipToKneeLineLeg3,...
+        hipToKneeLineLeg4,kneeToAnkleLineLeg1, kneeToAnkleLineLeg2,...
+        kneeToAnkleLineLeg3,kneeToAnkleLineLeg4, ankleToFeetLineLeg1,...
+        ankleToFeetLineLeg2, ankleToFeetLineLeg3, ankleToFeetLineLeg4];
+    %% 
+    % draw, save for gif and pause
+    drawnow
+    frame=getframe(fig);
+    im=frame2im(frame);
+    [imind,cm]=rgb2ind(im,256);
+    if i==1
+        imwrite(imind,cm,filename,'gif','Loopcount',inf);
+    else
+        imwrite(imind,cm,filename,'gif','WriteMode','append');
+    end
+    
+    pause(0.01);
+    %% 
+    % Delete the previous points and lines unless its the last time point
+    if i ~= length(timeMat)
+        for k=1:length(jntPointArr)
+            delete(jntPointArr(k))
+        end
+        for k=1:length(lineArr)
+            delete(lineArr(k))
+        end
+    end
+end
+end
